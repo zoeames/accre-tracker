f3c2b17f88412b87bb233d6b6428b449c39ea15f
Zoe Ames <zoeames@gmail.com>
2017-06-10 18:46:26 -0500

    auth page done

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,6 +1,7 @@
-app.controller("AuthCtrl", function($scope, AuthFactory){
+app.controller("AuthCtrl", function($location, $scope, AuthFactory){
  	$scope.loginUserGoogle = () => {
  		AuthFactory.authenticateGoogle($scope.auth).then(() => {
+ 			$location.url("/list");
  		}).catch((error) => {
  			console.log(error);
  		});
